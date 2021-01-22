b43dcde6c1363a334dad0fdc84762cdca2230835
Manuel Bärenz <m.baerenz@sonnen.de>
2021-01-22 12:52:47 +0100

    Give Accum existential clock type

--- src/accum.rs
+++ src/accum.rs
@@ -2,26 +2,28 @@ use crate::*;
 
 use std::marker::PhantomData;
 
-pub struct Accum<Acc, Input, F> {
+pub struct Accum<Acc, Input, C, F> {
     acc: Acc,
     f: F,
     phantom: PhantomData<Input>,
+    phantom_c: PhantomData<C>,
 }
 
-impl<Acc, Input, F: FnMut(&mut Acc, Input)> Accum<Acc, Input, F> {
+impl<Acc, Input, C, F: FnMut(&mut Acc, Input)> Accum<Acc, Input, C, F> {
     pub fn new(acc: Acc, f: F) -> Self {
         Accum {
             acc,
             f,
             phantom: PhantomData,
+            phantom_c: PhantomData,
         }
     }
 }
 
-impl<Acc: Clone, Input, F: FnMut(&mut Acc, Input)> StreamFunction for Accum<Acc, Input, F> {
+impl<Acc: Clone, Input, C, F: FnMut(&mut Acc, Input)> StreamFunction for Accum<Acc, Input, C, F> {
     type Input = Input;
     type Output = Acc;
-    type Clock = ();
+    type Clock = C;
 
     fn step(&mut self, input: Self::Input, _: Self::Clock) -> Self::Output {
         let acc = self.acc.clone();
